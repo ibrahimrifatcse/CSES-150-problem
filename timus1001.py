70e16587381de66108248c84430c673619b4ec6b
IBRAHIM REFAT <101995954+ibrahimrifatcse@users.noreply.github.com>
2023-08-08 19:26:17 +0600

    Create timus1001.py

---
+++ timus1001.py
@@ -0,0 +1,12 @@
+import sys
+import math
+
+nums = []
+
+for line in sys.stdin:
+    for word in line.split():
+        nums.append(float(word))
+nums.reverse()
+
+for num in nums:
+    print("%.4f" % math.sqrt(num))
